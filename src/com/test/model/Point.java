931336b7b2ba11026051f2f25359e780b3054d42
shezhanjun <shezhanjun@qq.com>
2020-02-08 00:38:59 +0800

    Create Point.java

---
+++ src/com/test/model/Point.java
@@ -0,0 +1,31 @@
+package com.test.model;
+/**
+ * @Description 点
+ * @Author 湛神
+ * @Date 2020/2/7 6:21 PM
+ */
+public class Point {
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
