a2a1a636be02f35672be807692fe0350e579f6fa
shezhanjun <shezhanjun@qq.com>
2020-02-08 00:31:34 +0800

    添加控制静态文件
    添加控制静态文件

---
+++ src/com/test/Constants.java
@@ -0,0 +1,35 @@
+package com.test;
+
+/**
+ * @Description 自定义常量区域
+ * @Author 湛神
+ * @Date 2020/2/7 6:18 PM
+ */
+public class Constants {
+
+    /**
+     * 初始感染数量
+     */
+    public static int ORIGINAL_COUNT = 50;
+    /**
+     * 传播率
+     */
+    public static float BROAD_RATE = 0.8f;
+    /**
+     * 潜伏时间
+     */
+    public static float SHADOW_TIME = 140;
+    /**
+     * 医院收治响应时间
+     */
+    public static int HOSPITAL_RECEIVE_TIME = 100;
+    /**
+     * 医院床位
+     */
+    public static int BED_COUNT = 250;
+    /**
+     * 流动意向平均值
+     */
+    public static float u = -0.99f;
+
+}
